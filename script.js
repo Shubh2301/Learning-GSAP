3e11b688053ec86a04ba93f4f2ee7c3c1b2700ce
Shubham Kamble <shubhamkamble3011@gmail.com>
2025-07-19 22:47:44 +0530

    Moving Cursor

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 let main=document.querySelector("#main");
 let cursor=document.querySelector("#cursor");
+let imageDiv=document.querySelector("#image");
 
 main.addEventListener("mousemove",function(dets){
    gsap.to(cursor,{
     x:dets.x,
     y:dets.y,
     duration:1,
-    ease:"back.out"
+    // ease:"back.out"
    })
     
+})
+
+imageDiv.addEventListener("mouseenter",function(){
+    cursor.innerHTML="View More"
+    gsap.to(cursor,{
+        scale:2
+    })    
+})
+imageDiv.addEventListener("mouseleave",function(){
+    cursor.innerHTML=""
+    gsap.to(cursor,{
+        scale:1
+    })    
 })
